13faad0f4dc56cc27c99bbd983b234c30e974141
prince gautam <55788541+Princegautam741@users.noreply.github.com>
2023-12-19 13:31:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,22 +1,8 @@
-<!DOCTYPE html>
-<html>
-
-<head>
-  <title>Metaverse Example</title>
-</head>
-
-<body>
-  <p id="status">Enter the Metaverse</p>
-  <button id="enterBtn">Enter</button>
-
-  <script>
+document.addEventListener("DOMContentLoaded", function () {
     const button = document.getElementById("enterBtn");
     const para = document.getElementById("status");
 
     button.addEventListener("click", () => {
-      para.innerText = "Entered Metaverse";
+        para.innerText = "Entered Metaverse";
     });
-  </script>
-</body>
-
-</html>
+});
